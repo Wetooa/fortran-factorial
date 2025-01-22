8bd80c943387827ad4dfc2f00c1988db77654a0d
wetooa <derpykidyt@gmail.com>
2025-01-22 11:28:00 +0800

    fixed factorial, added iterative and recursive cases

--- factorial.f90
+++ factorial.f90
@@ -1,23 +1,46 @@
 
 PROGRAM Factorial
     IMPLICIT NONE
-    INTEGER :: n, result
+    INTEGER :: n, iterativeResult, recursiveResult
 
-    PRINT *, "Enter a number to calculate its factorial:"
+    PRINT *, "Enter a number to calculate its factorial: "
     READ *, n
 
-    result = FactorialFunction(n)
-    PRINT *, "The factorial of", n, "is", result
+    iterativeResult = IterativeFactorialFunction(n)
+    recursiveResult = RecursiveFactorialFunction(n)
+
+    PRINT *, "The factorial of ", n
+    PRINT *, "Iterative: ", iterativeResult
+    PRINT *, "Recursive: ", recursiveResult
+
+
+CONTAINS
+
+  ! Iterative
+  FUNCTION IterativeFactorialFunction(num) RESULT(fact)
+      IMPLICIT NONE
+      INTEGER, INTENT(IN) :: num
+      INTEGER :: fact, i
+
+      fact = 1
+      DO i = 1, num
+          fact = fact * i
+      END DO
+  END FUNCTION IterativeFactorialFunction
+
+  ! Recursive
+  RECURSIVE FUNCTION RecursiveFactorialFunction(num) RESULT(fact)
+      IMPLICIT NONE
+      INTEGER, INTENT(IN) :: num
+      INTEGER :: fact
+
+      IF (num <= 1) THEN
+        fact = 1
+      ELSE
+        fact = RecursiveFactorialFunction(num - 1) * num
+      END IF
+
+  END FUNCTION RecursiveFactorialFunction
+
+
 END PROGRAM Factorial
-
-! Function to calculate factorial
-FUNCTION FactorialFunction(num) RESULT(fact)
-    IMPLICIT NONE
-    INTEGER, INTENT(IN) :: num
-    INTEGER :: fact, i
-
-    fact = 1
-    DO i = 1, num
-        fact = fact * i
-    END DO
-END FUNCTION FactorialFunction
---
+++ hello-world.f90
@@ -0,0 +1,8 @@
+PROGRAM Hello
+  ! Comments use !
+  IMPLICIT NONE
+
+  REAL :: real_number = 10;
+
+  PRINT *, "Hello, World!", real_number, "Why is it tabbed!"
+END PROGRAM Hello
