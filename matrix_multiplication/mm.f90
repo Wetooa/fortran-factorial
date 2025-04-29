77de631d22135305fd47aff59e35b8277b84d6a6
wetooa <derpykidyt@gmail.com>
2025-04-29 23:35:23 +0800

    Added matrix multiplication

--- matrix_multiplication/mm.f90
+++ matrix_multiplication/mm.f90
@@ -1,14 +1,38 @@
 
-PROGRAM sqrt_sum
-  IMPLICIT NONE
-  INTEGER, PARAMETER :: N = 100000000
-  INTEGER :: i
-  REAL(8) :: sum
 
-  sum = 0.0D0
+PROGRAM matrix_multiplication
+  IMPLICIT NONE
+
+  INTEGER, PARAMETER :: N = 3   ! Size of the matrices
+  INTEGER :: i, j, k
+  REAL :: A(N, N), B(N, N), C(N, N)
+
+  A = RESHAPE([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0], [N, N])
+  B = RESHAPE([9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0], [N, N])
+  C = 0.0
+
+  ! Matrix multiplication: C = A * B
   DO i = 1, N
-    sum = sum + SQRT(REAL(i, 8))
+    DO j = 1, N
+      DO k = 1, N
+        C(i, j) = C(i, j) + A(i, k) * B(k, j)
+      END DO
+    END DO
   END DO
 
-  PRINT *, "Sum:", sum
-END PROGRAM sqrt_sum
+  PRINT *, "Matrix A:"
+  DO i = 1, N
+    PRINT *, A(i, :)
+  END DO
+
+  PRINT *, "Matrix B:"
+  DO i = 1, N
+    PRINT *, B(i, :)
+  END DO
+
+  PRINT *, "Matrix C (A * B):"
+  DO i = 1, N
+    PRINT *, C(i, :)
+  END DO
+
+END PROGRAM matrix_multiplication
